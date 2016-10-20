8c581df5843874e988722395a055277fe3818a67
Jeremy <jeremy.c.mittleman.17@dartmouth.edu>
2016-10-20 17:39:14 -0400

    Front Screen Static V1

---
+++ UI/Assets/PinTopRight.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinTopRight : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+//		gameObject.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(.9f, .9f, Camera.main.farClipPlane)); 
+	}
+
+}
