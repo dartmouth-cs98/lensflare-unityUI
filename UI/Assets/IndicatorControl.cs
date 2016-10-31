80a90ae45c8d2d890bfe50246b8d2da2d2727262
Jeremy <jeremy.c.mittleman.17@dartmouth.edu>
2016-10-30 22:24:20 -0400

    spinning indicatoricon

---
+++ UI/Assets/IndicatorControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IndicatorControl : MonoBehaviour {
+	private bool spinning = false;
+	private Animator animator;
+
+	void Start() {
+		animator = GetComponent<Animator> ();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown ("o")) {
+			print ("O is pressed - turn on");
+			setIndicator (true);
+		} else if (Input.GetKeyDown ("p")) {
+			print ("P is pressed- turn off");
+			setIndicator (false);
+		}
+	}
+
+	public void setIndicator(bool a) {
+		spinning = a;
+		animator.SetBool ("Spinning", a);
+	}
+		
+}
