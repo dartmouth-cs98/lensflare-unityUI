e579b9ea2714ee80b9d906f80e52e4b16c2babe6
Armin Mahban <armin17@cs.dartmouth.edu>
2016-10-31 17:03:24 -0400

    Animation and start styling box

---
+++ UI/Assets/Rotate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public float speed = 100f;
+	public bool indicator_visible = true;
+
+	Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("t")) {
+			indicator_visible = !indicator_visible;
+		}
+
+		if (indicator_visible && renderer.enabled) {
+			transform.Rotate (Vector3.up, speed * Time.deltaTime);
+		} else if (indicator_visible && !renderer.enabled) {
+			renderer.enabled = true;
+		}
+		else {
+			renderer.enabled = false;
+		}
+	}
+}
--- UI/Assets/Scripts/PinTopLeft.cs
+++ UI/Assets/Scripts/PinTopLeft.cs
@@ -5,7 +5,7 @@ public class PinTopLeft : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(.9f, .9f, Camera.main.farClipPlane)); 
+		gameObject.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(.94f, .9f, Camera.main.farClipPlane)); 
 	}
 
 }
