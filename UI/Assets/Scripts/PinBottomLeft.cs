0c3f6d5a643437678bc43d9233678cd75e83299f
Armin Mahban <armin17@cs.dartmouth.edu>
2016-11-01 14:26:21 -0400

    Fix loading indicator pinning issues

--- UI/Assets/Scripts/PinBottomLeft.cs
+++ UI/Assets/Scripts/PinBottomLeft.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class PinBottomLeft : MonoBehaviour {
 
-		// Update is called once per frame
-		void Update () {
-			gameObject.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(.9f, .1f, Camera.main.farClipPlane)); 
-		}
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(.9f, .1f, Camera.main.farClipPlane)); 
+	}
 
 }
--- UI/Assets/Scripts/PinTopLeft.cs
+++ UI/Assets/Scripts/PinTopLeft.cs
@@ -6,6 +6,8 @@ public class PinTopLeft : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		gameObject.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(.94f, .9f, Camera.main.farClipPlane)); 
+		gameObject.transform.localEulerAngles = Camera.main.transform.localEulerAngles;
+		gameObject.transform.Rotate (new Vector3 (90, 0, 0));
 	}
 
 }
