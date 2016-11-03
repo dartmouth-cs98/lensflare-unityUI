188605089ac6459ae526f7e150e9f10537076ddf
Armin Mahban <armin17@cs.dartmouth.edu>
2016-11-03 16:55:07 -0400

    Image panel start (hidden atm)

--- UI/Assets/Scripts/ImageTester.cs
+++ UI/Assets/Scripts/ImageTester.cs
@@ -9,17 +9,14 @@ public class ImageTester : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		Texture2D text = new Texture2D (400, 287); 
+		Texture2D text = new Texture2D (0, 0); 
 		text.LoadImage(File.ReadAllBytes("./Assets/Images/eiffel-tower.jpg"));
-		text.Resize (300,300);
-		text.Apply (); 
-		GetComponentInChildren<Image> ().sprite = Sprite.Create (text, new Rect (0, 0, 300, 300), new Vector2 (.5f, .5f), 100);
+		TextureScale.Bilinear (text, 300, 200);
+		GetComponentInChildren<Image> ().sprite = Sprite.Create (text, new Rect (0, 0, 300, 200), new Vector2 (.5f, .5f), 100);
 
 
 
-
-
-	}
+	} 
 	
 	// Update is called once per frame
 	void Update () {
